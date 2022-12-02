901b2946538b41da4129d804257ba83d289eb8c7
seongyeon-kim <niforances@gmail.com>
2022-12-02 18:06:20 +0900

    update: Day01 initial

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,38 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: String): Int {
+
+        //split by \n\n
+        val list = input.split("\n\n")
+            .map {
+                it.split("\n")
+                    .map { it.toInt() }
+                    .sum()
+            }
+
+        return list.max()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: String): Int {
+
+        val list = input.split("\n\n")
+            .map {
+                it.split("\n")
+                    .map { it.toInt() }
+                    .sum()
+            }
+            .sorted()
+
+        return list.subList(list.size - 3, list.size).sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
-    val input = readInput("Day01")
+    val testInput = readInputRaw("Day01_test")
+    val input = readInputRaw("Day01")
+
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
+
     println(part1(input))
     println(part2(input))
 }
